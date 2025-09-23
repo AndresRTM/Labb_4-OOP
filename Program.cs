942933394eff5e4b312532ca9b2731715b9ad9fb
AndresRTM <andres-llano@hotmail.com>
2025-09-23 23:01:38 +0200

    Add Triangle class with area and perimeter calculations
    Implemented a new `Triangle` class in `Triangle.cs` that includes properties for the triangle's sides and methods to calculate the area using Heron's formula and the perimeter. Updated `Program.cs` to instantiate two `Triangle` objects and display their areas alongside existing circle functionality.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@
             Circle circle2 = new Circle(5);
             Console.WriteLine(circle1.GetArea());
             Console.WriteLine(circle2.GetArea());
+            Triangle triangle1 = new Triangle(3, 4, 5);
+            Triangle triangle2 = new Triangle(6, 8, 10);    
+            Console.WriteLine(triangle1.GetArea());
+            Console.WriteLine(triangle2.GetArea());
+            
+
         }
     }
 }
---
+++ Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_4_OOP
+{
+    internal class Triangle
+    {
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+        public double GetArea()
+        {
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+        public double GetPerimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+    }
+}
