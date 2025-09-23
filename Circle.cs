6a6d848c476df2756754732132d7cfb16a8b4e9a
AndresRTM <andres-llano@hotmail.com>
2025-09-23 22:47:44 +0200

    Added more methods

--- Circle.cs
+++ Circle.cs
@@ -19,6 +19,17 @@ namespace Labb_4_OOP
         {
             return 2 * Radius * Math.PI;
         }
+
+        public double  GetDiameter()
+        {
+            return Radius * 2;
+        }
+
+        public double GetVolume()
+        {
+            return (4/3) * Math.PI * Math.Pow(Radius, 3);
+        }
+
         public double Radius { get; set; }
 
         }
