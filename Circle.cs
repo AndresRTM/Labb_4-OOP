262f16ace2fb515a7c51e54a2bc6e68f67a435ec
AndresRTM <andres-llano@hotmail.com>
2025-09-23 22:39:57 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Labb_4_OOP
+{
+    internal class Circle
+    {
+        public Circle(double radius) 
+        {
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Radius * 2 * Math.PI;
+        }
+
+        public double Radius { get; set; }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Labb_4_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(5);
+            Console.WriteLine(circle1.GetArea());
+            Console.WriteLine(circle2.GetArea());
+        }
+    }
+}
